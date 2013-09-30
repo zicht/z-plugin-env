23f0757ff0d9d76e6cd21c1c2da3f5f88d3e73e8
Philip Bergman <philip@zicht.nl>
2013-09-30 11:54:38 +0000

    added port forward
    git-svn-id: svn://zon/projecten/zicht-libraries/z-plugins/branches/1.0/env@25690 0f35748b-8228-47f0-bbea-889afbbd9a42

--- Plugin.php
+++ Plugin.php
@@ -9,14 +9,39 @@
 namespace Zicht\Tool\Plugin\Env;
 
 use \Zicht\Tool\Plugin as BasePlugin;
-use Zicht\Tool\Container\Container;
+use \Zicht\Tool\Container\Container;
+use \Symfony\Component\Process\Process;
 use \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
 class Plugin extends BasePlugin
 {
+
+    /**
+     * Configures the env parameters
+     *
+     * @param \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition $rootNode
+     * @return mixed|void
+     */
+    public function appendConfiguration(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode
+            ->children()
+                ->arrayNode('portforward')
+                    ->children()
+                        ->scalarNode('local')->end()
+                        ->scalarNode('remote')->end()
+                        ->scalarNode('host')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+
+
     public function setContainer(Container $container)
     {
-        $container->method('env.versionat', function($container, $env, $verbose = false) {
+
+        $container->method('env.versionat', function(Container $container, $env, $verbose = false) {
             static $envVcsInfo = array();
 
             if (!isset($envVcsInfo[$env])) {
@@ -38,8 +63,231 @@ class Plugin extends BasePlugin
                 return $container->call('vcs.versionid', $envVcsInfo[$env]);
             }
         });
+
         $container->decl('env.ssh.connectable', function($container) {
             return shell_exec(sprintf('ssh -oBatchMode=yes %s "echo 1" 2>/dev/null;', $container->resolve('env.ssh')));
         });
+
+        $container->decl('env.portforward', function(Container $container) {
+
+
+            $bindParam     = null;
+            $remotePorts   = array();
+            $localPorts    = array();
+            $hosts         = array();
+            $skipPortCheck = false;
+
+            if ($container->has('port_remote')) {
+
+                $remotePort = $container->resolve('port_remote');
+                $host       = $container->resolve('host');
+
+                if($container->has('port_local')){
+                    $localPort  = $container->resolve('port_local');
+                }else{
+                    // none given set port to non privileged port < 1024
+                    $localPort  = $container->resolve('port_remote') + 2000;
+
+                    if(!$container->has('explain')){
+                        $container->output->writeln(
+                            sprintf(
+                                "Binding remote port <comment>%s</comment> to local port to <comment>%s</comment> on <comment>%s</comment>",
+                                $remotePort,
+                                $localPort,
+                                $host
+                            )
+                        );
+                    }
+                }
+                // check if port is used else check next one
+                while ($this->checkLocalPort($localPort, $container) === false){
+                    $localPort++;
+                }
+
+                $bindParam .= sprintf('-L %s:%s:%s ', $localPort, $host, $remotePort);
+
+            }else{
+
+                if ($container->has('portforward.remote')) {
+                    $remotePorts = $this->processPortString($container->resolve('portforward.remote'));
+                }
+
+                if ($container->has('portforward.local')) {
+                    $localPorts = $this->processPortString($container->resolve('portforward.local'));
+
+                    if(count($localPorts) !== count($remotePorts)){
+                        throw new \InvalidArgumentException('Defined local and remote ports does not match, check settings');
+                    }
+                } else {
+                    // we do the port check here so
+                    // no need to check later.
+                    $skipPortCheck = true;
+
+                    foreach($remotePorts as $id => $remotePort){
+                        // none given set port to non privileged port < 1024
+                        // and check if port is used else check next one
+                        $localPort = $remotePort + 2000;
+
+                        while ($this->checkLocalPort($localPort, $container) === false){
+                            $localPort++;
+                        }
+
+                        $localPorts[$id] = $localPort;
+                    }
+                }
+
+                if ($container->has('portforward.host')) {
+
+                    $hosts = array_map(
+                        'trim',
+                        explode(
+                            ',',
+                            $container->resolve('portforward.host')
+                        )
+                    );
+
+                    if(count($hosts) !== count($remotePorts)){
+                        throw new \InvalidArgumentException('Defined host(s) does not match defined remote port(s), check settings');
+                    }
+                } else {
+                    foreach($remotePorts as $id => $remotePort){
+                        $hosts[$id] = 'localhost';
+                    }
+                }
+
+                if ( $skipPortCheck === false ){
+                    foreach($localPorts as $id => $localPort){
+
+                        if($container->has('verbose')){
+
+                            $container->output->writeln(
+                                sprintf(
+                                    "Checking if system is listening to port %s",
+                                    $localPort
+                                )
+                            );
+
+                        }
+
+                        if($this->checkLocalPort($localPort, $container) === false){
+                            throw new \Symfony\Component\Process\Exception\RuntimeException(
+                                sprintf(
+                                    'System already listening to port %s, try using a different port',
+                                    $localPort
+                                )
+                            );
+                        }
+
+                    }
+                }
+
+                foreach(array_keys($remotePorts) as $id){
+                    $bindParam .= sprintf('-L %s:%s:%s ', $localPorts[$id], $hosts[$id], $remotePorts[$id]);
+                }
+            }
+
+
+
+            return $bindParam;
+
+        });
+
+
+    }
+
+    /**
+     * Check if given port is used
+     *
+     * On match ( and port is used ) it will
+     * return something like :
+     * tcp6  0  0 :::80 :::* LISTEN
+     * So we need to check for empty string
+     *
+     * @param $port
+     * @param \Zicht\Tool\Container\Container $container
+     * @return bool
+     * @throws \UnexpectedValueException
+     */
+    private function checkLocalPort($port, Container $container){
+
+        $return = false;
+        $cmd    = sprintf(
+            "netstat -lnt | awk '$6 == \"LISTEN\" && $4 ~ \".%s\"'",
+            $port
+        );
+
+
+        if ($container->resolve('explain')) {
+
+            $container->output->writeln(sprintf('# Check if port %s is used',$port));
+
+            $container->output->writeln(
+                sprintf(
+                    '( %s );',
+                    rtrim(
+                        trim($cmd),
+                        "\n"
+                    )
+                )
+            );
+
+            $return = true;
+
+
+        } else {
+
+            $process    = new Process($cmd);
+
+            $process->run();
+
+            $exitCode = $process->getExitCode();
+            $stdOut   = $process->getOutput();
+
+            if($exitCode > 0){
+                throw new \UnexpectedValueException(
+                    sprintf(
+                        "Checking if port was used failed [%s] %s",
+                        $exitCode,
+                        $process->getErrorOutput()
+                    )
+                );
+            }
+
+            $return = empty($stdOut);
+        }
+
+
+
+        return $return;
+    }
+
+    /**
+     * will check if given var is numeric, and splits given string on comma
+     *
+     * @param $ports
+     * @return array
+     * @throws \UnexpectedValueException
+     */
+    private function processPortString($ports){
+
+        $ports = explode(',', $ports);
+
+        array_walk($ports, function(&$v){
+
+                if(\is_numeric($v)){
+                    $v = (int) trim($v);
+                }else{
+                    throw new \UnexpectedValueException(
+                        sprintf(
+                            '%s is not a valid port',
+                            $v
+                        )
+                    );
+                }
+
+            }
+
+        );
+        return $ports;
     }
 }
